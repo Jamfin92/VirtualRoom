0204f1a6810ea234deaf5c964f2b5426cc9e8a39
Jamfin92 <36896551+Jamfin92@users.noreply.github.com>
2021-05-24 17:55:09 -0400

    Cube Stand-In Implemented
    Adds dropdown menus for date and location selection for US States and months from April 2020 to January 2021.
    
    Cubes represent COVID-19 totals and deaths in blue and red respectively.

---
+++ VirtualRoom_V_01/JSONReader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class JSONReader : MonoBehaviour
+{
+    public TextAsset jsonFile;
+
+    void Start()
+    {
+        States statesInJson = JsonUtility.FromJson<States>(jsonFile.text);
+
+        foreach (State state in statesInJson.states)
+        {
+            Debug.Log("State/Province: " + state.Province_State + " Confirmed Cases: " + state.Confirmed + " Confirmed Deaths: " + state.Deaths);
+        }
+    }
+}
